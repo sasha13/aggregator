b8ea94055c6bbc828ee09f9b550ed221de877603
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-17 14:40:46 +0100

    start of login/register pages

--- app/Controller/Login/Login.php
+++ app/Controller/Login/Login.php
@@ -12,10 +12,46 @@ class Login {
   }
 
   public function index() {
-    die($this->data['3rd_party']);
+    print '
+      <html>
+        <head></head>
+        <title>Aggregator</title>
+        <body>
+          <div>
+            <h2>Aggregator</h2>
+          </div>
+          <div>
+            <ul>
+              <li><a href="login">Login</a></li>
+              <li><a href="register">Register</a></li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Rss</a></li>
+              <li><a href="#">Bookmarks</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>Login</h3>
+            <form action="login/process" method="post">
+              Username: <input type="text" name="Username" /><p>
+              Password: <input type="password" name="Password" />
+              <br>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </body>
+      </html>
+    ';
   }
 
   public function view() {
     var_dump($this->data);
+  }
+
+  public function process() {
+    var_dump($_POST);
   }
 }
--- app/Controller/Register/Register.php
+++ app/Controller/Register/Register.php
@@ -12,10 +12,47 @@ class Register {
   }
 
   public function index() {
-    die($this->data['3rd_party']);
+    print '
+      <html>
+        <head></head>
+        <title>Aggregator</title>
+        <body>
+          <div>
+            <h2>Aggregator</h2>
+          </div>
+          <div>
+            <ul>
+              <li><a href="login">Login</a></li>
+              <li><a href="register">Register</a></li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Rss</a></li>
+              <li><a href="#">Bookmarks</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>Register</h3>
+            <form action="register/process" method="post">
+              Username: <input type="text" name="Username" /><p>
+              Password: <input type="password" name="Password" />
+              <br>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </body>
+      </html>
+    ';
   }
 
   public function view() {
     var_dump($this->data);
   }
+
+  public function process() {
+    var_dump($_POST);
+  }
+
 }
