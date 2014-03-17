b6e1d259554da64a27968591e554ccd5f094a021
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-17 14:42:56 +0100

    class for rendering html

--- app/Html/Html.php
+++ app/Html/Html.php
@@ -5,13 +5,13 @@ namespace Html;
 
 class Html {
 
-  private $layout;
   private $view_name;
   private $data;
   private $controllerName;
 
   public function __construct() {}
 
-  private function render($layout, $view_name, array $data) {}
-  private function render_partial($view_name, array $data) {}
+  public static function render($view_name, array $data) {
+
+  }
 }
