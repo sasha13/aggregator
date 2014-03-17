65571f9e8db1a9cadd49671fbfa3606455eb3f57
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-17 14:43:50 +0100

    testing db connection

--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 require '../vendor/autoload.php';
 
 use Router\Router;
+use Db\Db;
+
+$conn = Db::getConnection();
+//$sql = "SELECT * FROM user";
+//$q = $conn->query($sql);
+//var_dump($q->fetch(PDO::FETCH_OBJ));
 
 $router = new Router($_SERVER['REQUEST_URI']);
 
