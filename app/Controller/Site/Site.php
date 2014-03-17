c2256a057c34b7ee25a05cce62ffb6521dfab063
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-17 14:41:51 +0100

    start of homepage

--- app/Controller/Site/Site.php
+++ app/Controller/Site/Site.php
@@ -14,8 +14,42 @@ class Site extends Main {
     //Html::layout = 'main';
     $a = $this->getControllerName($this);
     var_dump(dirname(__DIR__));
+    var_dump($a);
 //     $model = loadModel('model_name');
 //     Html::render('view_name', array());
+    print '
+      <html>
+        <head></head>
+        <title>Aggregator</title>
+        <body>
+          <div>
+            <h2>Aggregator</h2>
+          </div>
+          <div>
+            <ul>
+              <li><a href="login">Login</a></li>
+              <li><a href="#">Register</a></li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Rss</a></li>
+              <li><a href="#">Bookmarks</a></li>
+            </ul>
+          </div>
+          <div>
+            <h3>Aggregator</h3>
+          </div>
+          <div>
+            <h3>Aggregator</h3>
+          </div>
+          <div>
+            <h3>Aggregator</h3>
+          </div>
+        </body>
+      </html>
+    ';
   }
 
 }
