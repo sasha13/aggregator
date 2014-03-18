87444da04b42672eb7b3a78110c3c2ae8bf8cce8
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 15:54:40 +0100

    added db insert new user to db functionality

--- app/Controller/Register/Register.php
+++ app/Controller/Register/Register.php
@@ -4,6 +4,7 @@ namespace Controller\Register;
 
 use Controller\Main\Main;
 use Html\Html;
+use Db\Db;
 
 
 class Register extends Main {
@@ -15,8 +16,9 @@ class Register extends Main {
   public function index() {
     $controllerName = $this->getControllerNameLowercase();
 //     $model = loadModel('model_name');
+    $data = array('title' => 'Aggggregator');
     $html = new Html($controllerName);
-    $html->render('index', array('username' => 'sasha'));
+    $html->render('index', $data);
   }
 
   public function view() {
@@ -25,6 +27,27 @@ class Register extends Main {
 
   public function process() {
     var_dump($_POST);
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $salt = 'th1rt33n';
+      $saltedPassword = md5($salt . $password);
+
+      $conn = Db::getConnection();
+      $sql = "INSERT INTO user (username, password) VALUES (:username, :password)";
+      $q = $conn->prepare($sql);
+      $q->execute(array(':username' => $username, ':password' => $saltedPassword));
+
+      if (!$q) {
+        die("Execute query error, because: ". $conn->errorInfo());
+      } else {
+        echo 'Registration succesfull.';
+        header('Location: /');
+      }
+
+    } else {
+      die('Username and password must be entered.');
+    }
   }
 
 }
