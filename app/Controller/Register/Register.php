736db5af541bf70e8c32e02499180b08f868317e
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 10:49:27 +0100

    moved html to template file
    moved html to temoplate file
    
    moved html to tpl file

--- app/Controller/Register/Register.php
+++ app/Controller/Register/Register.php
@@ -2,50 +2,21 @@
 
 namespace Controller\Register;
 
+use Controller\Main\Main;
+use Html\Html;
 
-class Register {
 
-  private $data;
+class Register extends Main {
 
   public function __construct($data) {
     $this->data = $data;
   }
 
   public function index() {
-    print '
-      <html>
-        <head></head>
-        <title>Aggregator</title>
-        <body>
-          <div>
-            <h2>Aggregator</h2>
-          </div>
-          <div>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="login">Login</a></li>
-              <li><a href="register">Register</a></li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Rss</a></li>
-              <li><a href="#">Bookmarks</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Register</h3>
-            <form action="register/process" method="post">
-              Username: <input type="text" name="Username" /><p>
-              Password: <input type="password" name="Password" />
-              <br>
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-        </body>
-      </html>
-    ';
+    $controllerName = $this->getControllerNameLowercase();
+//     $model = loadModel('model_name');
+    $html = new Html($controllerName);
+    $html->render('index', array('username' => 'sasha'));
   }
 
   public function view() {
