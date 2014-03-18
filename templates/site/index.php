d073b1c6b8340adf7b32b21d7a894cdf0dcb3743
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 10:14:03 +0100

    added new template files

---
+++ templates/site/index.php
@@ -0,0 +1,32 @@
+<html>
+  <head></head>
+  <title>Aggregator</title>
+  <body>
+    <div>
+      <h2>Aggregator</h2>
+    </div>
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="login">Login</a></li>
+        <li><a href="register">Register</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Rss</a></li>
+        <li><a href="#">Bookmarks</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>Register</h3>
+      <form action="register/process" method="post">
+        Username: <input type="text" name="Username" /><p>
+        Password: <input type="password" name="Password" />
+        <br>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </body>
+</html>
