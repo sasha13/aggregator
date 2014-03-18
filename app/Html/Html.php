5175b7402fe3b2e27b115235e611e5d949537cbd
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 15:51:26 +0100

    simulating template engine

--- app/Html/Html.php
+++ app/Html/Html.php
@@ -14,9 +14,19 @@ class Html {
   }
 
   public function render($viewName, array $data) {
-    //var_dump(TEMPLATE_DIR);
-    //var_dump($data);
     $fileToRender = TEMPLATE_DIR . $this->controllerName . '/' . $viewName . '.php';
-    echo file_get_contents($fileToRender);
+    //echo file_get_contents($fileToRender);
+
+    if (!file_exists($fileToRender)) {
+      return "Error loading template file ($fileToRender).";
+    }
+
+    $output = file_get_contents($fileToRender);
+    foreach ($data as $key => $value) {
+      $tagToReplace = "{@$key}";
+      $output = str_replace($tagToReplace, $value, $output);
+    }
+    echo $output;
   }
+
 }
