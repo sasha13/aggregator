882d57faea434292c7254e3d85d03a7003441172
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 10:20:43 +0100

    added home link in menu

--- app/Controller/Login/Login.php
+++ app/Controller/Login/Login.php
@@ -22,6 +22,7 @@ class Login {
           </div>
           <div>
             <ul>
+              <li><a href="/">Home</a></li>
               <li><a href="login">Login</a></li>
               <li><a href="register">Register</a></li>
             </ul>
