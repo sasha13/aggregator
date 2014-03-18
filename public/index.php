96bbf6908f53eb673631ca12489a2d7162522b30
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-18 10:23:16 +0100

    defined root_dir and tpl_dir constants

--- public/index.php
+++ public/index.php
@@ -5,6 +5,13 @@ require '../vendor/autoload.php';
 use Router\Router;
 use Db\Db;
 
+define('ROOT_DIR', dirname(__DIR__));
+define('TEMPLATE_DIR', ROOT_DIR . '/templates/');
+
+//var_dump(TEMPLATE_DIR);
+//var_dump(ROOT_DIR);
+//die();
+
 $conn = Db::getConnection();
 //$sql = "SELECT * FROM user";
 //$q = $conn->query($sql);
