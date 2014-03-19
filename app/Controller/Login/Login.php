8887510df396c3caa644101641570a169020513a
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-19 11:46:36 +0100

    creating login page

--- app/Controller/Login/Login.php
+++ app/Controller/Login/Login.php
@@ -2,50 +2,23 @@
 
 namespace Controller\Login;
 
+use Controller\Main\Main;
+use Html\Html;
+use Db\Db;
 
-class Login {
 
-  private $data;
+class Login extends Main {
 
   public function __construct($data) {
     $this->data = $data;
   }
 
   public function index() {
-    print '
-      <html>
-        <head></head>
-        <title>Aggregator</title>
-        <body>
-          <div>
-            <h2>Aggregator</h2>
-          </div>
-          <div>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="login">Login</a></li>
-              <li><a href="register">Register</a></li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Rss</a></li>
-              <li><a href="#">Bookmarks</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Login</h3>
-            <form action="login/process" method="post">
-              Username: <input type="text" name="Username" /><p>
-              Password: <input type="password" name="Password" />
-              <br>
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
-        </body>
-      </html>
-    ';
+    $controllerName = $this->getControllerNameLowercase();
+//     $model = loadModel('model_name');
+    $data = array('title' => 'Aggregator');
+    $html = new Html($controllerName);
+    $html->render('index', $data);
   }
 
   public function view() {
---
+++ templates/login/index.php
@@ -0,0 +1,32 @@
+<html>
+  <head></head>
+  <title>Aggregator</title>
+  <body>
+    <div>
+      <h2>{@title}</h2>
+    </div>
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="login">Login</a></li>
+        <li><a href="register">Register</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Rss</a></li>
+        <li><a href="#">Bookmarks</a></li>
+      </ul>
+    </div>
+    <div>
+      <h3>Login</h3>
+      <form action="login/process" method="post">
+        Username: <input type="text" name="username" /><p>
+        Password: <input type="password" name="password" />
+        <br>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  </body>
+</html>
