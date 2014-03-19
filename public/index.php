148b669adbfdbe20bbd2a3450373c9cfc06d7c8e
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-19 17:10:43 +0100

    cleaning code

--- public/index.php
+++ public/index.php
@@ -8,14 +8,5 @@ use Db\Db;
 define('ROOT_DIR', dirname(__DIR__));
 define('TEMPLATE_DIR', ROOT_DIR . '/templates/');
 
-//var_dump(TEMPLATE_DIR);
-//var_dump(ROOT_DIR);
-//die();
-
-//$conn = Db::getConnection();
-//$sql = "SELECT * FROM user";
-//$q = $conn->query($sql);
-//var_dump($q->fetch(PDO::FETCH_OBJ));
-
 $router = new Router($_SERVER['REQUEST_URI']);
 
