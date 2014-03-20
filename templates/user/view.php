5a1d7db2108ab499dbd712b204e9c19257ef97eb
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-20 16:04:58 +0100

    added tpl for user page

---
+++ templates/user/view.php
@@ -0,0 +1,22 @@
+<html>
+  <head></head>
+  <title>Aggregator</title>
+  <body>
+    <div>
+      <h2>{@title}</h2>
+    </div>
+    <div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="logout">Logout</a></li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Rss</a></li>
+        <li><a href="#">Bookmarks</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
