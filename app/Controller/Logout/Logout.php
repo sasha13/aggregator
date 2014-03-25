35b64a141dfb5d3acccd2528e2709cde9127ba1b
Aleksandar Sotirovic <aleksandar.sotirovic@diwanee.com>
2014-03-25 17:14:44 +0100

    added logout ctrl

---
+++ app/Controller/Logout/Logout.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controller\Logout;
+
+use Controller\Main\Main;
+use Utils\Utils;
+
+class Logout extends Main {
+
+  //public function __construct($data) {
+  //  $this->data = $data;
+  //}
+
+  public function index() {
+    session_start();
+    setcookie(session_name(), '', time() - 42000);
+    unset($_SESSION);
+    session_destroy();
+    Utils::redirect("/");
+    //var_dump(session_name());
+  }
+
+}
